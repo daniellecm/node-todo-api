92e9ad5a1648609908a59b926d0012d41f26bdb9
Campos Monteiro <dcamposm@indra.es>
2019-01-10 15:47:15 -0200

    create user + tokens

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,6 @@ mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 module.exports = { mongoose }
